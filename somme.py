f2e32efd9e52db765fc3b5e941d0e335b3cd24c9
ogandandaga <sogandaga12@gmail.com>
2023-05-06 11:31:29 +0000

    first commit

---
+++ somme.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  6 10:07:51 2023
+
+@author: SBS
+"""
+
+nbr1= input('entrer le premier nombre')
+nbr2= input('entrer le premier nombre')
+
+s = int(nbr1) + int(nbr2)
+
+print('la somme de {0}et {1} est {2}'.format(nbr1, nbr2,s))
